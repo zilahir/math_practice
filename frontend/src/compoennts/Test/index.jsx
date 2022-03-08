cf05b184ba019fddcd3f4684589ad469995bba25
Richard Zilahi <richard.zilahi@varjo.com>
2022-03-08 22:21:48 +0200

    dev env fix

---
+++ frontend/src/compoennts/Test/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./test.module.scss";
+
+function Test() {
+  return (
+    <div className={styles.container}>
+      <p>
+        hello
+      </p>
+    </div>
+  );
+}
+
+export default Test;
