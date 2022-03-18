9abe1058781e4b0a2fed33bfb39f0134f8aa36e5
Richard Zilahi <richard.zilahi@varjo.com>
2022-03-18 19:41:06 +0200

    mysql class init

---
+++ backend/src/database/MySQL.js
@@ -0,0 +1,30 @@
+import mysql from 'mysql';
+
+class MySQL {
+    constructor(host, user, database, password) {
+        this.host = host;
+        this.user = user;
+        this.password = password;
+        this.database = database;
+        this.connection = mysql.createConnection({
+            host: this.host,
+            user: this.user,
+            database: this.database
+        });
+    }
+
+    convertQueryResultToObject(queryResult) {
+        return JSON.parse((JSON.stringify(queryResult)))
+    }
+
+    query(queryToExecute) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(queryToExecute, (err, result) => {
+                if (err) reject(err)
+                resolve(this.convertQueryResultToObject(result))
+            })
+        })
+    }
+}
+
+export default MySQL;
