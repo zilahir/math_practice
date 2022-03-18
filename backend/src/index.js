d7632e063430a269103ff9f1b89e517a58e2679b
Richard Zilahi <richard.zilahi@varjo.com>
2022-03-18 11:05:40 +0200

    fixed eslint for server side

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const app = require("./server");
 const { port } = require("./config");
 
 const server = app.listen(port, function() {
-  console.log("Webserver is ready");
+  console.info("Webserver is ready");
 });
 
 //
