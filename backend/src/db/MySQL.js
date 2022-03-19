a74486aec44e4fb52e3700886f51818e9a5b9adc
Richard Zilahi <richard.zilahi@varjo.com>
2022-03-20 00:05:47 +0200

    querying mysql now works

--- backend/src/db/MySQL.js
+++ backend/src/db/MySQL.js
@@ -1,23 +1,28 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 
 class MySQL {
-    constructor(host, user, database, password) {
+    constructor(host, port, user, password, database) {
         this.host = host;
         this.user = user;
         this.password = password;
         this.database = database;
+        this.port = port
         this.connection = mysql.createConnection({
             host: this.host,
             user: this.user,
-            database: this.database
+            password: this.password,
+            database: this.database,
+            port: this.port,
         });
     }
 
     convertQueryResultToObject(queryResult) {
+        console.log("queryResult", queryResult);
         return JSON.parse((JSON.stringify(queryResult)))
     }
 
     query(queryToExecute) {
+        console.log("queryToExecute", queryToExecute);
         return new Promise((resolve, reject) => {
             this.connection.query(queryToExecute, (err, result) => {
                 if (err) reject(err)
--- backend/src/test/controller.js
+++ backend/src/test/controller.js
@@ -1,5 +1,13 @@
-export function test(request, response) {
+import { serverConfig } from "../config"
+import MySQL from "../db/MySQL"
+
+const { host, port, user, password } = serverConfig.database
+
+const database = new MySQL(host, port, user, password, "test")
+
+export async function test(request, response) {
+    const testQuery = await database.query('SELECT * FROM table_1');
     response.status(200).send({
-        isSuccess: true,
+        queryResult: testQuery,
     })
 }
