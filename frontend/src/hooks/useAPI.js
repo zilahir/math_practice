7b7bfa13fed51bdafb5b72a68e862c7a6368c9f9
Richard Zilahi <richard.zilahi@varjo.com>
2022-03-28 11:27:11 +0300

    comment for useApi

--- frontend/src/hooks/useAPI.js
+++ frontend/src/hooks/useAPI.js
@@ -1,6 +1,26 @@
 import { useState, useCallback } from "react";
 import api from "../api";
 
+/*
+A hooks reactban olyan pure function amiben hasznalhatunk react specifickus
+funkciokat, ujrafelhasznalhato modon. (Pl useState, useEffect, stb.)
+
+Ebben a hookban 3 stat van (loding, apiResponse, error).
+
+Maga a funcion nagyon egyszerű, egy objecttel tér vissza amiben 4 property van:
+
+loading, apiReponse, error, apiRequestHandler
+
+Az apiRequestHandler az egyetlen member function  hookban, ez felelős az API hívásért.
+
+Egyetlen paramétert vár, ami egy objektum, egy pathName property-vel, amivel meghadhatjuk, hogy
+az apiRequestHandler hívásakor melyik endpointot hívjuk.
+
+Etőtl lesz újrafelhasználható, és nevezhetjük hooknak, mert bármelyik komponensben importálhatjuk
+akkor annyi instance lesz belőle amennyiszer initializálva van.
+
+*/
+
 function useApi({ pathName, method }) {
   const [loading, isLoading] = useState(false);
   const [apiReponse, setApiResposne] = useState(undefined);
