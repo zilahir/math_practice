f9b4ec025eca1fec8d9ebd42752c4b5658d4fdf7
Richard Zilahi <richard.zilahi@varjo.com>
2022-04-01 19:22:55 +0300

    added prettier + new eslint config

--- frontend/src/hooks/useAPI.js
+++ frontend/src/hooks/useAPI.js
@@ -1,56 +1,47 @@
 import { useState, useCallback } from "react";
 import api from "../api";
 
-/*
-A hooks reactban olyan pure function amiben hasznalhatunk react specifickus
-funkciokat, ujrafelhasznalhato modon. (Pl useState, useEffect, stb.)
-
-Ebben a hookban 3 stat van (loding, apiResponse, error).
-
-Maga a funcion nagyon egyszerű, egy objecttel tér vissza amiben 4 property van:
-
-loading, apiReponse, error, apiRequestHandler
-
-Az apiRequestHandler az egyetlen member function  hookban, ez felelős az API hívásért.
-
-Egyetlen paramétert vár, ami egy objektum, egy pathName property-vel, amivel meghadhatjuk, hogy
-az apiRequestHandler hívásakor melyik endpointot hívjuk.
-
-Etőtl lesz újrafelhasználható, és nevezhetjük hooknak, mert bármelyik komponensben importálhatjuk
-akkor annyi instance lesz belőle amennyiszer initializálva van.
-
-*/
-
+/**
+ * @param root0
+ * @param root0.pathName
+ * @param root0.method
+ */
 function useApi({ pathName, method }) {
   const [loading, isLoading] = useState(false);
   const [apiReponse, setApiResposne] = useState(undefined);
   const [error, setError] = useState(undefined);
 
-  const apiRequestHandler = useCallback(async (body) => {
-    isLoading(true);
-    if (method === "POST") {
-      try {
-        const response = await api.post(pathName, {
-          ...body,
-        });
-        setApiResposne(response);
-      } catch (apiError) {
-        setError(apiError);
+  const apiRequestHandler = useCallback(
+    async (body) => {
+      isLoading(true);
+      if (method === "POST") {
+        try {
+          const response = await api.post(pathName, {
+            ...body,
+          });
+          setApiResposne(response);
+        } catch (apiError) {
+          setError(apiError);
+        }
+        isLoading(false);
+      } else {
+        try {
+          const response = await api.get(pathName);
+          setApiResposne(response);
+        } catch (apiError) {
+          setError(apiError);
+        }
+        isLoading(false);
       }
-      isLoading(false);
-    } else {
-      try {
-        const response = await api.get(pathName);
-        setApiResposne(response);
-      } catch (apiError) {
-        setError(apiError);
-      }
-      isLoading(false);
-    }
-  }, [pathName, method]);
+    },
+    [pathName, method]
+  );
 
   return {
-    loading, apiReponse, error, apiRequestHandler,
+    loading,
+    apiReponse,
+    error,
+    apiRequestHandler,
   };
 }
 
