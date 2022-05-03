aa0d849d3ca5c91b217489cb8b6cf4771d53e770
Richard Zilahi <richard.zilahi@varjo.com>
2022-05-03 22:33:04 +0300

    sql query for getting tasks

--- backend/src/tasks/tasks.controller.js
+++ backend/src/tasks/tasks.controller.js
@@ -19,3 +19,30 @@ export async function createNewTask(request, response) {
 
   response.status(200).send(sqlQueryResult);
 }
+
+/**
+ * @param request
+ * @param response
+ */
+export async function getAllTasks(request, response) {
+  const sqlQuery =
+    "select t.id, t.task_image_id, t.category_id, t.period_id, t.task_no, t.task_point_no, ti.filePath, c.name as 'categoryName', p.periodName from tasks t INNER JOIN categories c on c.id = t.category_id INNER JOIN periods p on p.id = t.period_id INNER JOIN task_images ti on ti.id = t.task_image_id";
+
+  const sqlQueryResult = await database.query(sqlQuery);
+
+  response.status(200).send(sqlQueryResult);
+}
+
+/**
+ * @param request
+ * @param response
+ */
+export async function getTaskById(request, response) {
+  const { taskId } = request.params;
+
+  const sqlQuery = `select t.id, t.task_image_id, t.category_id, t.period_id, t.task_no, t.task_point_no, ti.filePath, c.name as "categoryName", p.periodName from tasks t INNER JOIN categories c on c.id = t.category_id INNER JOIN periods p on p.id = t.period_id INNER JOIN task_images ti on ti.id = t.task_image_id WHERE t.id = "${taskId}";`;
+
+  const task = await database.query(sqlQuery);
+
+  response.status(200).send(task);
+}
--- backend/src/tasks/tasks.routes.js
+++ backend/src/tasks/tasks.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
-import { createNewTask } from "./tasks.controller";
+import { createNewTask, getAllTasks, getTaskById } from "./tasks.controller";
 
 const router = Router();
 
 router.post("/", [createNewTask]);
+
+router.get("/all", [getAllTasks]);
+
+router.get("/:taskId", [getTaskById]);
 
 export default router;
