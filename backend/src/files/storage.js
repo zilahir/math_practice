1591b2a1dc695ce7736e3cadcff9a354548a1a9d
Richard Zilahi <richard.zilahi@varjo.com>
2022-09-06 17:50:34 +0300

    s3 image upload debug

--- backend/src/files/storage.js
+++ backend/src/files/storage.js
@@ -1,9 +1,9 @@
 import path from "path";
 import multer from "multer";
-import AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 import multerS3 from "multer-s3";
 
-const s3 = new AWS.S3({
+const s3 = new S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: "us-east-1",
